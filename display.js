d7f8471bdf2ee53a432f3e7b14d5afdc46212d33
Tomasz Cienkowski <tomaszcienkowski@Tomaszs-MacBook-Air.local>
2020-08-07 15:28:57 +0200

    first commit

---
+++ display.js
@@ -0,0 +1,23 @@
+function displayFileInput() {
+  document.querySelector("#home").style.display = "None"
+  document.querySelector("#fileinput").style.display = "Block"
+  document.querySelector("#report").style.display = "None"
+  enableButton()
+}
+
+function displayReport() {
+  document.querySelector("#file-input").style.display = "None"
+  document.querySelector("#report").style.display = "Block"
+}
+  
+function displayHome() {
+  location.reload()
+}
+
+function enableButton() {
+  if (fileContentsA.length == 2 && fileContentsB.length == 2) {
+    document.querySelector("#results").disabled = false;
+    } else {
+    document.querySelector("#results").disabled = true;
+    }
+}
